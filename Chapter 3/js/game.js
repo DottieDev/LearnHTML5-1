6cbbdfb325b643cd31d5ac04e5a9a737299db93b
Dorothy Lee <dcldeveloper@comcast.net>
2017-11-19 21:38:09 +0000

    trying out some form features

--- Chapter 3/js/game.js
+++ Chapter 3/js/game.js
@@ -1,275 +1,279 @@
 // JavaScript File
-    function Player(query) {
-      var element = document.querySelector(query);
-      var trackWidth = parseInt(element.parentElement.offsetWidth);
-      var minLeft = 0 - parseInt(element.offsetWidth / 2);
-      var maxLeft = trackWidth - parseInt(element.offsetWidth / 2);
+function Player(query) {
+  var element = document.querySelector(query);
+  var trackWidth = parseInt(element.parentElement.offsetWidth);
+  var minLeft = 0 - parseInt(element.offsetWidth / 2);
+  var maxLeft = trackWidth - parseInt(element.offsetWidth / 2);
 
-      function getLeft() {
-        return parseInt(element.style.left);
-      }
+  function getLeft() {
+    return parseInt(element.style.left);
+  }
 
-      this.getMinLeft = function() {
-        return minLeft;
-      };
+  this.getMinLeft = function() {
+    return minLeft;
+  };
 
-      this.getMaxLeft = function() {
-        return maxLeft;
-      };
+  this.getMaxLeft = function() {
+    return maxLeft;
+  };
 
-      this.move = function() {
-        var left = getLeft();
-        var speed = parseInt(element.attributes.getNamedItem("data-speed").value);
+  this.move = function() {
+    var left = getLeft();
+    var speed = parseInt(element.attributes.getNamedItem("data-speed").value);
 
-        element.style.left = (left + speed) + "px";
+    element.style.left = (left + speed) + "px";
 
-        if (getLeft() > maxLeft) {
-          this.moveToFinish();
-        }
-        else if (getLeft() < minLeft) {
-          this.moveToStart();
-        }
-      };
-
-      this.moveToPercent = function(percent) {
-        element.style.left = parseInt(percent * maxLeft) + "px";
-
-        if (percent >= 100) {
-          this.moveToFinish();
-        }
-        else if (percent <= 0) {
-          this.moveToStart();
-        }
-      };
-
-      this.isFinished = function() {
-        return parseInt(element.style.left) >= maxLeft;
-      };
-
-      this.moveToStart = function() {
-        element.style.left = this.getMinLeft() + "px";
-      };
-
-      this.moveToFinish = function() {
-        element.style.left = this.getMaxLeft() + "px";
-      };
-
-      this.setSpeed = function(speed) {
-        return element.attributes.getNamedItem("data-speed").value = speed;
-      };
+    if (getLeft() > maxLeft) {
+      this.moveToFinish();
     }
-
-    var hero = new Player("[data-name='goodGuy']");
-    var enemy = new Player("[data-name='badGuy']");
-
-    var tickPeriod = 500;
-    var gamesWon = 0;
-
-    var wordsToWrite = document.querySelector("#wordsToWrite");
-    var wordsWritten = document.querySelector("#wordsWritten");
-    var words = "";
-    var currentLetter = 0;
-
-    function isGameOver() {
-      return hero.isFinished() || enemy.isFinished();
+    else if (getLeft() < minLeft) {
+      this.moveToStart();
     }
+  };
 
-    function tick() {
+  this.moveToPercent = function(percent) {
+    element.style.left = parseInt(percent * maxLeft) + "px";
 
-      hero.move();
-      enemy.move();
-
-      if (isGameOver()) {
-        document.body.removeEventListener("keydown", handleKeyPress);
-
-        if (hero.isFinished()) {
-          gamesWon++;
-          showWinPanel();
-        }
-        else if (enemy.isFinished()) {
-          showLosePanel();
-        }
-      }
-      else {
-        setTimeout(tick, tickPeriod);
-      }
+    if (percent >= 100) {
+      this.moveToFinish();
     }
-
-    function showWinPanel() {
-      var panel = document.querySelector("#messageContainer");
-      var leaderboard = document.querySelector("#messageContainer article section");
-      var championForm = document.querySelector("#messageContainer form");
-      var header = document.querySelector("#messageContainer h2");
-
-      header.innerText = "You win the boat!";
-
-      panel.classList.add("open");
-
-      setTimeout(function() {
-        championForm.classList.remove("closed");
-        leaderboard.classList.remove("closed");
-      }, 3000);
+    else if (percent <= 0) {
+      this.moveToStart();
     }
+  };
 
-    function showLosePanel() {
-      var panel = document.querySelector("#messageContainer");
-      var leaderboard = document.querySelector("#messageContainer article section");
-      var championForm = document.querySelector("#messageContainer form");
-      var header = document.querySelector("#messageContainer h2");
+  this.isFinished = function() {
+    return parseInt(element.style.left) >= maxLeft;
+  };
 
-      header.innerText = "You lose. Try Again.";
+  this.moveToStart = function() {
+    element.style.left = this.getMinLeft() + "px";
+  };
 
-      panel.classList.add("open");
+  this.moveToFinish = function() {
+    element.style.left = this.getMaxLeft() + "px";
+  };
 
-      setTimeout(function() {
-        leaderboard.classList.remove("closed");
-        championForm.classList.add("closed");
-      }, 3000);
+  this.setSpeed = function(speed) {
+    return element.attributes.getNamedItem("data-speed").value = speed;
+  };
+}
+
+var hero = new Player("[data-name='goodGuy']");
+var enemy = new Player("[data-name='badGuy']");
+
+var tickPeriod = 500;
+var gamesWon = 0;
+
+var wordsToWrite = document.querySelector("#wordsToWrite");
+var wordsWritten = document.querySelector("#wordsWritten");
+var words = "";
+var currentLetter = 0;
+
+function isGameOver() {
+  return hero.isFinished() || enemy.isFinished();
+}
+
+function tick() {
+
+  hero.move();
+  enemy.move();
+
+  if (isGameOver()) {
+    document.body.removeEventListener("keydown", handleKeyPress);
+
+    if (hero.isFinished()) {
+      gamesWon++;
+      showWinPanel();
     }
-
-    function saveNewChampion(event) {
-      event.preventDefault();
-
-      var name = this.elements.namedItem("fullName").value;
-      var email = this.elements.namedItem("email").value;
-
-      var champions = document.querySelector("#messageContainer ul");
-      var rank = champions.children.length + 1;
-      var difficulty = document.querySelector("#navOptions input[type='range']").value;
-
-      var stars = "";
-      for (var i = 0; i < difficulty; i++)
-        stars += "*";
-
-      if (rank > gamesWon)
-        return false;
-
-      var newChampion = document.createElement("li");
-      newChampion.innerHTML = "<p>#" + rank + "</p>" +
-        "<p>" + name + " " + stars + "</p>" +
-        "<p><a href='mailto:" + email + "'>" + email + "</a></p>";
-
-      champions.appendChild(newChampion);
+    else if (enemy.isFinished()) {
+      showLosePanel();
     }
+  }
+  else {
+    setTimeout(tick, tickPeriod);
+  }
+}
 
-    function setEnemySpeed(speed) {
-      enemy.setSpeed(speed * 2);
-    }
+function showWinPanel() {
+  var panel = document.querySelector("#messageContainer");
+  var leaderboard = document.querySelector("#messageContainer article section");
+  var championForm = document.querySelector("#messageContainer form");
+  var header = document.querySelector("#messageContainer h2");
 
-    function handleKeyPress(event) {
+  header.innerText = "You win the boat!";
 
-      var wordsLen = wordsWritten.children.length;
-      if (event.keyCode == 16)  // shift
-        return false;
+  panel.classList.add("open");
 
-      if (event.keyCode == 8) {    //backspace
-        event.preventDefault();
+  setTimeout(function() {
+    championForm.classList.remove("closed");
+    leaderboard.classList.remove("closed");
+  }, 3000);
+}
 
-        if (wordsLen < 1)
-          return false;
+function showLosePanel() {
+  var panel = document.querySelector("#messageContainer");
+  var leaderboard = document.querySelector("#messageContainer article section");
+  var championForm = document.querySelector("#messageContainer form");
+  var header = document.querySelector("#messageContainer h2");
 
-        wordsWritten.children[wordsLen - 1].remove();
-        return false;
-      }
+  header.innerText = "You lose. Try Again.";
 
-      var letter = String.fromCharCode(event.keyCode);
+  panel.classList.add("open");
 
-      if (!event.shiftKey && event.keyCode >= 65 && event.keyCode <= 90)
-        letter = String.fromCharCode(event.keyCode + 32);
+  setTimeout(function() {
+    leaderboard.classList.remove("closed");
+    championForm.classList.add("closed");
+  }, 3000);
+}
 
-      if (event.keyCode == 188)
-        letter = ",";
+function saveNewChampion(event) {
+  event.preventDefault();
 
-      if (event.keyCode == 190)
-        letter = ".";
+  var name = this.elements.namedItem("fullName").value;
+  var email = this.elements.namedItem("email").value;
+  var scheduleDate = this.elements.namedItem("scheduleDate").value;
+  var faveColor = this.elements.namedItem("faveColor").value;
 
-      if (letter == words[wordsLen]) {
-        wordsWritten.innerHTML += "<span class='correct'>" + letter + "</span>";
-        var correct = document.querySelectorAll("#wordsWritten .correct").length;
-        var percent = correct / words.length;
-        hero.moveToPercent(percent);
-      }
-      else {
-        wordsWritten.innerHTML += "<span class='wrong'>" + letter + "</span>";
-      }
+  var champions = document.querySelector("#messageContainer ul");
+  var rank = champions.children.length + 1;
+  var difficulty = document.querySelector("#navOptions input[type='range']").value;
 
+  var stars = "";
+  for (var i = 0; i < difficulty; i++)
+    stars += "*";
+
+  if (rank > gamesWon)
+    return false;
+
+  var newChampion = document.createElement("li");
+  newChampion.innerHTML = "<p>#" + rank + "</p>" +
+    "<p>" + name + " " + stars + "</p>" +
+    "<p>Next game: " + scheduleDate + "</p>" +
+    "<p><a href='mailto:" + email + "'>" + email + "</a></p>";
+
+  champions.appendChild(newChampion);
+  this.style.backgroundColor = faveColor;
+  
+}
+
+function setEnemySpeed(speed) {
+  enemy.setSpeed(speed * 10);
+}
+
+function handleKeyPress(event) {
+
+  var wordsLen = wordsWritten.children.length;
+  if (event.keyCode == 16) // shift
+    return false;
+
+  if (event.keyCode == 8) { //backspace
+    event.preventDefault();
+
+    if (wordsLen < 1)
       return false;
-    }
 
-    function getWords() {
-      var phrases = [
-        "The quick orange fox stopped being quick once its hunter started wearing chrome.",
-        "Hi. I must win my dream boat before the snaughty Mr. Snaughtington takes it from me.",
-        "Hi.I love HTML5 because it is such an amazing platform in which to develop games.",
-        "Hi. People who type fast are more likely to win at this game. Yes, that is true.",
-        "Hi.For some reason, I think I can have fun while typing, and improving at the same time."
-      ];
-      var rand = parseInt(Math.random() * phrases.length);
+    wordsWritten.children[wordsLen - 1].remove();
+    return false;
+  }
 
-      return phrases[rand];
-    }
+  var letter = String.fromCharCode(event.keyCode);
 
-    function doOnPlayClicked() {
-      var panel = document.querySelector("#messageContainer");
-      var leaderboard = document.querySelector("#messageContainer article section");
-      var championForm = document.querySelector("#messageContainer form");
-      var difficulty = document.querySelector("#navOptions input[type='range']");
-      var playBtn = document.querySelector("#mainContainer button[data-intent='play']");
+  if (!event.shiftKey && event.keyCode >= 65 && event.keyCode <= 90)
+    letter = String.fromCharCode(event.keyCode + 32);
 
-      words = getWords();
-      wordsToWrite.innerText = words;
-      wordsWritten.innerText = "";
+  if (event.keyCode == 188)
+    letter = ",";
 
-      currentLetter = 0;
+  if (event.keyCode == 190)
+    letter = ".";
 
-      if (playBtn)
-        playBtn.remove();
+  if (letter == words[wordsLen]) {
+    wordsWritten.innerHTML += "<span class='correct'>" + letter + "</span>";
+    var correct = document.querySelectorAll("#wordsWritten .correct").length;
+    var percent = correct / words.length;
+    hero.moveToPercent(percent);
+  }
+  else {
+    wordsWritten.innerHTML += "<span class='wrong'>" + letter + "</span>";
+  }
 
-      panel.classList.remove("open");
-      leaderboard.classList.add("closed");
-      championForm.classList.add("closed");
+  return false;
+}
 
-      hero.moveToStart();
-      enemy.moveToStart();
-      setEnemySpeed(difficulty.value);
-      hero.setSpeed(0);
+function getWords() {
+  var phrases = [
+    "The quick orange fox stopped being quick once its hunter started wearing chrome.",
+    "I must win my dream boat before the bad guy takes it from me.",
+    "I love HTML5 because it is such an amazing platform in which to develop games.",
+    "People who type fast are more likely to win at this game.",
+    "I think I can have fun while typing, and improving at the same time."
+  ];
+  var rand = parseInt(Math.random() * phrases.length);
 
-      document.body.addEventListener("keydown", handleKeyPress);
-      setTimeout(tick, tickPeriod);
-    }
+  return phrases[rand];
+}
 
-    function main() {
-      // Make options navigation toggle when clicked
-      var options = document.querySelector("#navOptions img");
-      options.addEventListener("click", function(event) {
-        event.preventDefault();
+function doOnPlayClicked() {
+  var panel = document.querySelector("#messageContainer");
+  var leaderboard = document.querySelector("#messageContainer article section");
+  var championForm = document.querySelector("#messageContainer form");
+  var difficulty = document.querySelector("#navOptions input[type='range']");
+  var playBtn = document.querySelector("#mainContainer button[data-intent='play']");
 
-        if (options.parentElement.classList.contains("open"))
-          options.parentElement.classList.remove("open");
-        else
-          options.parentElement.classList.add("open");
-      });
+  words = getWords();
+  wordsToWrite.innerText = words;
+  wordsWritten.innerText = "";
 
-      // Update difficulty value when slider changes
-      var difficulty = document.querySelector("#navOptions input[type='range']");
-      difficulty.addEventListener("change", function(event) {
-        event.preventDefault();
+  currentLetter = 0;
 
-        var difficultyIndicator = document.querySelector("#navOptions div p span");
-        difficultyIndicator.innerText = this.value;
+  if (playBtn)
+    playBtn.remove();
 
-        setEnemySpeed(this.value);
-      });
+  panel.classList.remove("open");
+  leaderboard.classList.add("closed");
+  championForm.classList.add("closed");
 
-      // Handle new champion submission
-      var newChampionForm = document.querySelector("#messageContainer form");
-      newChampionForm.addEventListener("submit", saveNewChampion);
+  hero.moveToStart();
+  enemy.moveToStart();
+  setEnemySpeed(difficulty.value);
+  hero.setSpeed(0);
 
-      var playBtn = document.querySelectorAll("button[data-intent='play']");
+  document.body.addEventListener("keydown", handleKeyPress);
+  setTimeout(tick, tickPeriod);
+}
 
-      for (var i = 0, len = playBtn.length; i < len; i++)
-        playBtn[i].addEventListener("click", doOnPlayClicked);
-    }
+function main() {
+  // Make options navigation toggle when clicked
+  var options = document.querySelector("#navOptions img");
+  options.addEventListener("click", function(event) {
+    event.preventDefault();
 
-    main();
- 
+    if (options.parentElement.classList.contains("open"))
+      options.parentElement.classList.remove("open");
+    else
+      options.parentElement.classList.add("open");
+  });
+
+  // Update difficulty value when slider changes
+  var difficulty = document.querySelector("#navOptions input[type='range']");
+  difficulty.addEventListener("change", function(event) {
+    event.preventDefault();
+
+    var difficultyIndicator = document.querySelector("#navOptions div p span");
+    difficultyIndicator.innerText = this.value;
+
+    setEnemySpeed(this.value);
+  });
+
+  // Handle new champion submission
+  var newChampionForm = document.querySelector("#messageContainer form");
+  newChampionForm.addEventListener("submit", saveNewChampion);
+
+  var playBtn = document.querySelectorAll("button[data-intent='play']");
+
+  for (var i = 0, len = playBtn.length; i < len; i++)
+    playBtn[i].addEventListener("click", doOnPlayClicked);
+}
+
+main();
